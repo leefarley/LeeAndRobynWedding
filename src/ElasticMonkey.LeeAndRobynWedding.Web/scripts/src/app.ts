899657c43faf33d88f5041dbfa02837e1b3721d5
Lee Farley <lee.farley@live.com>
2014-06-08 01:16:00 +1200

    added in directives to run timer

--- src/ElasticMonkey.LeeAndRobynWedding.Web/scripts/src/app.ts
+++ src/ElasticMonkey.LeeAndRobynWedding.Web/scripts/src/app.ts
@@ -4,7 +4,7 @@
 var app : ng.IModule = angular.module("app", ["ngResource"]);
 
 
-app.controller("homeCtrl", ($scope, $http) => {
+app.controller("reservationCtrl", ($scope, $http)=> {
     $scope.submitRsvpCode = ()=> {
         var request = { "code": $scope.rsvpCode };
         $scope.isWorking = true;
@@ -22,13 +22,70 @@ app.controller("homeCtrl", ($scope, $http) => {
     $scope.people = [];
     $scope.isWorking = false;
     $scope.isFormLoaded = false;
-    $scope.areMarried = ()=> {
-        var theDate = new Date("Feburary 7, 2015 15:00:00");
-        var currentTime = new Date();
-        if (theDate < currentTime) {
-            return true;
+}).directive("timer", [
+    '$compile', ($compile)=> {
+        return {
+            restrict: 'E',
+            replace: false,
+            scope: {
+                targetDateString: "@"
+            },
+            template: "{{days}} Days &#3664; {{hhours}} Hours &#3664; {{mminutes }} Minutes &#3664; {{sseconds }} Seconds",
+            controller: ($scope, $element)=> {
+                $element.append($compile($element.contents())($scope));
+                $scope.sseconds = 0;
+                $scope.mminutes = 0;
+                $scope.hhours = 0;
+                $scope.days = 0;
+                //determine initial values of time units and add AddSeconds functionality
+                $scope.targetDate = Date.parse($scope.targetDateString);
+                var currentDate: any = new Date();
+                $scope.millis = $scope.targetDate - currentDate;
+
+                function calculateTimeUnits() {
+                    $scope.seconds = Math.abs(Math.floor(($scope.millis / 1000) % 60));
+                    $scope.minutes = Math.abs(Math.floor((($scope.millis / (60000)) % 60)));
+                    $scope.hours = Math.abs(Math.floor((($scope.millis / (3600000)) % 24)));
+                    $scope.days = Math.abs(Math.floor((($scope.millis / (3600000)) / 24)));
+
+                    //add leading zero if number is smaller than 10
+                    $scope.sseconds = $scope.seconds < 10 ? '0' + $scope.seconds : $scope.seconds;
+                    $scope.mminutes = $scope.minutes < 10 ? '0' + $scope.minutes : $scope.minutes;
+                    $scope.hhours = $scope.hours < 10 ? '0' + $scope.hours : $scope.hours;
+                };
+
+                calculateTimeUnits();
+
+                function tick() {
+                    var currentDate: any = new Date();
+                    $scope.millis = $scope.targetDate - currentDate;
+
+                    calculateTimeUnits();
+
+                    setTimeout(()=> {
+                        tick();
+                        $scope.$digest();
+                    }, 1000);
+                }
+
+                tick();
+            }
+        };
+    }
+]).directive("marriedHeader", function() {
+    return {
+        restrict: "A",
+        replace: false,
+        template: "{{text}}",
+        link: ($scope)=> {
+            var theDate = new Date("Feburary 7, 2015 15:00:00");
+            var currentTime = new Date();
+            if (theDate < currentTime) {
+                $scope.text = "Married for";
+            } else {
+                $scope.text = "We'll be married in";
+            }
         }
-        return false;
     };
 });
 
--- src/ElasticMonkey.LeeAndRobynWedding.Web/scripts/src/directives.js
+++ src/ElasticMonkey.LeeAndRobynWedding.Web/scripts/src/directives.js
@@ -1,1 +1,59 @@
-﻿//# sourceMappingURL=directives.js.map
+﻿app.directive("ng-countdown", function () {
+    return {
+        restrict: 'E',
+        replace: false,
+        scope: {
+            targetDateString: ""
+        },
+        transclude: true,
+        controller: [
+            '$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
+                // Checking for trim function since IE8 doesn't have it
+                // If not a function, create tirm with RegEx to mimic native trim
+                if (typeof String.prototype.trim !== 'function') {
+                    String.prototype.trim = function () {
+                        return this.replace(/^\s+|\s+$/g, '');
+                    };
+                }
+
+                //determine initial values of time units and add AddSeconds functionality
+                $scope.targetDate = new Date($scope.targetDateString);
+
+                function calculateTimeUnits() {
+                    $scope.seconds = Math.floor(($scope.millis / 1000) % 60);
+                    $scope.minutes = Math.floor((($scope.millis / (60000)) % 60));
+                    $scope.hours = Math.floor((($scope.millis / (3600000)) % 24));
+                    $scope.days = Math.floor((($scope.millis / (3600000)) / 24));
+
+                    //add leading zero if number is smaller than 10
+                    $scope.sseconds = $scope.seconds < 10 ? '0' + $scope.seconds : $scope.seconds;
+                    $scope.mminutes = $scope.minutes < 10 ? '0' + $scope.minutes : $scope.minutes;
+                    $scope.hhours = $scope.hours < 10 ? '0' + $scope.hours : $scope.hours;
+                }
+                ;
+
+                function tick() {
+                    var currentDate = new Date();
+                    $scope.millis = $scope.targetDate - currentDate;
+
+                    calculateTimeUnits();
+
+                    $scope.timeoutId = setTimeout(function () {
+                        tick();
+                        $scope.$digest();
+                    }, 1000);
+                }
+
+                function resetTimeout() {
+                    if ($scope.timeoutId) {
+                        clearTimeout($scope.timeoutId);
+                    }
+                }
+
+                resetTimeout();
+                tick();
+            }
+        ]
+    };
+});
+//# sourceMappingURL=directives.js.map
